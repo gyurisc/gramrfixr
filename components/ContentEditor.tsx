e644984d78a0f333315a3da09a9c5021e4f26eb7
gyurisc <gyurisc@gmail.com>
2023-03-24 19:48:54 +0100

    editor now works ok. end of day1

--- components/ContentEditor.tsx
+++ components/ContentEditor.tsx
@@ -19,38 +19,33 @@ function ContentEditor() {
 
     const highlightWords = ['error', 'mistake'];
 
-    const highlightedContent = content.split(' ').map((word, index) => {
-        if (highlightWords.includes(word.toLowerCase())) {
-            return (
-                `<span class="relative" key="${index}">
+    const highlightedContent = content
+        .split(' ')
+        .map((word, index) => {
+            console.log('highlightedWords: ', word, ' index: ', index);
+            if (highlightWords.includes(word.toLowerCase())) {
+                console.log('highlightedWords', ' includes ', word);
+                return (
+                    `<span class="relative" key="${index}">
               ${word}
               <span class="absolute left-0 bottom-[2px] w-full h-[2px] bg-red-500"></span>
             </span>`
-            );
-        }
-        return word + ' ';
-    });
+                );
+            }
+            return word + ' ';
+        });
 
 
     const contentMarkup = {
-        __html: highlightedContent.join('').replace(/(?:\r\n|\r|\n)/g, '<br>'),
+        __html: highlightedContent.join(''),
     };
 
-    // GPT says: 
-    // In the handleBlur function, I've updated the code to replace <div> tags (used by browsers for newlines in contentEditable elements) 
-    // with <br> tags and remove the closing </div> tags. This way, the newlines are preserved correctly when the component loses focus.
     const handleBlur = (event: React.FocusEvent<HTMLDivElement>) => {
-        const text = event.target.innerHTML
-            .replace(/<div>/g, '\n')
-            .replace(/<\/div>/g, '')
-            .replace(/<br>/g, '\n');
-
-        console.log('text with newlines fixed ', text);
-        setContent(text);
+        setContent(event.target.innerHTML);
     };
 
 
-    console.log('content Markup', contentMarkup);
+    console.log('content Markup: ', contentMarkup);
     return (
         <div
             className="border border-gray-300 p-4 min-h-[200px] w-full outline-none focus:ring focus:border-blue-300"
