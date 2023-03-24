d4800c858ce4215b556039241c63b59741d83d5e
Krisztian Gyuris <krisztian_gyuris@steris.com>
2023-03-24 06:36:52 +0100

    adding header and footer

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,24 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+  let description = "Let GramrFixr improve your text in seconds.";
+  let ogimage = "";
+  let sitename = "gramrfixr.com";
+  let title = "GramrFixr";
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content={description} />
+        <meta property="og:site_name" content={sitename} />
+        <meta property="og:description" content={description} />
+        <meta property="og:title" content={title} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta property="og:image" content={ogimage} />
+        <meta name="twitter:image" content={ogimage} />
+      </Head>
       <body>
         <Main />
         <NextScript />
