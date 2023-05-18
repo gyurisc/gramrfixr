181490ae966d529eaef3ed6d1519a8a9699db353
gyurisc <gyurisc@gmail.com>
2023-05-18 13:51:25 +0200

    adding simpleanalytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 import "@/styles/globals.css";
 
@@ -10,7 +10,15 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={inter.className}>
       <Component {...pageProps} />
-      <Analytics />
+      <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
+      <noscript>
+        {/* eslint-disable @next/next/no-img-element */}
+        <img
+          src="https://queue.simpleanalyticscdn.com/noscript.gif"
+          alt=""
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+      </noscript>
     </div>
   );
 }
